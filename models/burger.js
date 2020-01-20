c5a1abb1dca8ecbc5b1971b7af63d3cc66eaa184
EricVincitore <epv1001@verizon.net>
2020-01-19 22:54:31 -0500

    started to debug burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,24 @@
 var orm = require("../config/orm.js");
 
-var cat = {
-    all: function(cb) {
-      orm.all("cats", function(res) {
-        cb(res);
+var burger = {
+    all: function(dv) {
+      orm.all("burgers", function(res) {
+        dv(res);
       });
     },
     // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("cats", cols, vals, function(res) {
-        cb(res);
+    create: function(cols, vals, dv) {
+      orm.create("burgers", cols, vals, function(res) {
+        dv(res);
       });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("cats", objColVals, condition, function(res) {
-        cb(res);
+    update: function(objColVals, devoured, dv) {
+      orm.update("burgers", objColVals, devoured, function(res) {
+        dv(res);
       });
     }
   };
   
   // Export the database functions for the controller (catsController.js).
-  module.exports = cat;
+  module.exports = burger;
   
